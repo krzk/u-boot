23b4ad45fc3f4db141122f5aa4db97306cba5fb3
Jeroen Hofstee <jeroen@myspectrum.nl>
2013-08-10 17:16:50 +0200

    compiler_gcc: do not redefine __gnu_attributes
    gcc allows extensions to be non compiler specific by defining
    __* macros for the attributes supported by gcc. Having a
    different definition causes many warnings during the build
    (cdefs.h on FreeBSD uses __attribute((__pure__)) where u-boot
    uses __attribute__((pure)) for example). Do not redefine
    these macros to suppress these warnings.
    
    This patch ignores the checkpatch warning:
    WARNING: __packed is preferred over __attribute__((packed))
    
    Signed-off-by: Jeroen Hofstee <jeroen@myspectrum.nl>
    (cherry picked from commit 7ea50d52849fe8ffa5b5b74c979b60b1045d6fc9)
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -65,7 +65,7 @@
 
 #define __deprecated			__attribute__((deprecated))
 #ifndef __packed
-#define __packed			__attribute__((packed))
+# define __packed			__attribute__((packed))
 #endif
 #ifndef __weak
 #define __weak				__attribute__((weak))
@@ -96,10 +96,10 @@
  * [...]
  */
 #ifndef __pure
-#define __pure				__attribute__((pure))
+# define __pure				__attribute__((pure))
 #endif
 #ifndef __aligned
-#define __aligned(x)			__attribute__((aligned(x)))
+# define __aligned(x)			__attribute__((aligned(x)))
 #endif
 #define __printf(a, b)			__attribute__((format(printf, a, b)))
 #define __scanf(a, b)			__attribute__((format(scanf, a, b)))
